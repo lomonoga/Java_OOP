6f4b3d4be61cb6583e16ccd88a1a1558db518330
LMG <trintsa@mail.ru>
2023-02-27 00:41:34 +0500

    task 1.2

--- Program.java
+++ Program.java
@@ -1,5 +1,38 @@
+import java.text.DecimalFormat;
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        authorization();
+        printSineTable(45);
+    }
+
+    public static void authorization(){
+        System.out.print("Введите ваше Имя: ");
+        var scanner = new Scanner(System.in);
+        var name = scanner.nextLine();
+        if (!Objects.equals(name, "")) {
+            System.out.println("Здравствуйте, " + name + "!");
+        }
+        else {
+            System.out.println("Вы захотели остаться анонимным? Хорошо, мы уважаем наших клиентов");
+        }
+    }
+
+    public static void printSineTable(int endAngle){
+        var indentAngle = "      ";
+        var indentSine = "    ";
+        System.out.print("Угол:");
+        for (var angle = 0; angle <= endAngle; angle += 5){
+            System.out.print(indentAngle + angle);
+        }
+
+        System.out.print("\nСинус: ");
+        var decimalFormat = new DecimalFormat( "#.##" );
+        for (var angle = 0; angle <= endAngle; angle += 5){
+            var sine = decimalFormat.format(Math.sin(Math.toRadians(angle)));
+            System.out.print(indentSine + sine);
+        }
     }
 }
