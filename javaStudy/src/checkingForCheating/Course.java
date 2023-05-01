3c2eb076c46c984df77c9a0f9ea578e0587759d4
Lomonoga <trintsa@mail.ru>
2023-05-01 23:41:16 +0500

    smth changes

--- javaStudy/src/checkingForCheating/Course.java
+++ javaStudy/src/checkingForCheating/Course.java
@@ -8,23 +8,25 @@ import java.util.ArrayList;
 public class Course {
     private String mName;
     private ArrayList<Tuple3<Student, Assessment, Cheating>> mInformationAboutStudents;
-    private ArrayList<Task> mAllTasks;
+    private Integer mCountTasks;
+    private Integer mAverageTimePerTask;
 
     public String getName() {
         return mName;
     }
 
-    public Course(String name) {
+    public Course(String name, Integer CountTasks,Integer AverageTimePerTask) {
         this.mName = name;
         this.mInformationAboutStudents = new ArrayList<>();
-        this.mAllTasks = new ArrayList<>();
+        this.mCountTasks = CountTasks;
+        this.mAverageTimePerTask = AverageTimePerTask;
     }
 
     public void AddStudent(Student student) throws MyException {
         mInformationAboutStudents.add(new Tuple3<>(student, new Assessment(0), Cheating.Unknown));
     }
 
-    public void AddTask(Task task) {
-        mAllTasks.add(task);
+    public Integer GetAssessmentAndCheatingByName(String nameStudent) throws MyException {
+        ////////Пока заглушка
     }
 }
